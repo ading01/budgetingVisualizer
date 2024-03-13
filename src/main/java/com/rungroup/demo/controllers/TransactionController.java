b097fbdd8c871357fe9de844f72427596c3dc6d1
ading01 <allan.ding@yale.edu>
2024-03-12 21:37:18 -0400

    opened cors for transaction post endpoint

--- src/main/java/com/rungroup/demo/controllers/TransactionController.java
+++ src/main/java/com/rungroup/demo/controllers/TransactionController.java
@@ -18,6 +18,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/transactions")
+@CrossOrigin
 public class TransactionController {
 
     private final TransactionService transactionService;
